38c1752980a725e250270748f3f376700750891c
Jarle Nygård <jarle@famnygard.com>
2015-02-17 22:16:08 +0100

    Still not working
    Organized the tests better

--- proactima.jsonobject.tests/DescribeJsonObject.cs
+++ proactima.jsonobject.tests/DescribeJsonObject.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Polenter.Serialization;
 
 namespace proactima.jsonobject.tests
 {
@@ -435,39 +433,6 @@ namespace proactima.jsonobject.tests
 
             // t
             actual.Should().Be(expected, "Does not retain parentId");
-        }
-
-
-        [TestMethod]
-        public void ItShouldSerializeDt()
-        {
-            // g
-            var json = DateTime.UtcNow;
-
-            // w
-            var ms = UXSerializer.Serialize(json);
-
-            // t
-            var deserialized = UXSerializer.Deserialize<DateTime>(ms);
-            var jsonAsString = JsonConvert.SerializeObject(json);
-            var deserializedAsString = JsonConvert.SerializeObject(deserialized);
-            jsonAsString.Should().Be(deserializedAsString);
-        }
-
-        [TestMethod]
-        public void ItShouldSerialize()
-        {
-            // g
-            var json = JsonFactory.CreateComplexJsonObject();
-
-            // w
-            var ms = UXSerializer.Serialize(json);
-
-            // t
-            var deserialized = UXSerializer.Deserialize<JsonObject>(ms);
-            var jsonAsString = JsonConvert.SerializeObject(json);
-            var deserializedAsString = JsonConvert.SerializeObject(deserialized);
-            jsonAsString.Should().Be(deserializedAsString);
         }
 
         [TestMethod]
---
+++ proactima.jsonobject.tests/DescribeUXSerializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace proactima.jsonobject.tests
+{
+    [TestClass]
+    public class DescribeUXSerializer
+    {
+        [TestMethod]
+        public void ItShouldSerializeDt()
+        {
+            // g
+            var json = DateTime.UtcNow;
+
+            // w
+            var ms = UXSerializer.Serialize(json);
+
+            // t
+            var deserialized = UXSerializer.Deserialize<DateTime>(ms);
+            var jsonAsString = JsonConvert.SerializeObject(json);
+            var deserializedAsString = JsonConvert.SerializeObject(deserialized);
+            jsonAsString.Should().Be(deserializedAsString);
+        }
+
+        [TestMethod]
+        public void ItShouldSerialize()
+        {
+            // g
+            var json = JsonFactory.CreateComplexJsonObject();
+
+            // w
+            var ms = UXSerializer.Serialize(json);
+
+            // t
+            var deserialized = UXSerializer.Deserialize<JsonObject>(ms);
+            var jsonAsString = JsonConvert.SerializeObject(json);
+            var deserializedAsString = JsonConvert.SerializeObject(deserialized);
+            jsonAsString.Should().Be(deserializedAsString);
+        }
+    }
+}

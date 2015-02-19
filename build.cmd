635a4be481f11d1c18a44097ead52c9b94cf74b3
Anders Østhus <anders.osthus@outlook.com>
2015-02-19 09:07:40 +0100

    Now with build file

---
+++ build.cmd
@@ -0,0 +1,24 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+	set config=Release
+)
+
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+	set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=".nuget\nuget.exe"
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild proactima.jsonobject.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net451
+mkdir Build\lib\Portable-Win81+Wpa81
+
+%nuget% pack ".\proactima.jsonobject.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"

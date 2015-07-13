56d32009e87d8a79b01ebeb4888719e94e11223f
noocyte <jarle.nygaard@outlook.com>
2015-07-13 12:09:06 +0200

    Adding support for DataContract serialization

---
+++ proactima.jsonobject.tests/DescribeSerialization.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace proactima.jsonobject.tests
+{
+    public class DescribeSerialization
+    {
+        [Fact]
+        public void ItShouldDataContractSerialize_GivenComplexObject()
+        {
+            // g
+            var complexObject = new JsonObject
+            {
+                {"title", "the title"},
+                {"array", new[] {new JsonObject {{"title", "sub1"}}, new JsonObject {{"title", "sub2"}}}},
+                {
+                    "ref", new JsonObject
+                    {
+                        {"type", "sometype"},
+                        {"values", new[] {"1", "2"}}
+                    }
+                },
+            };
+
+            // w
+            var actual = WcfTestHelper.DataContractSerializationRoundTrip(complexObject);
+
+            // t
+            actual.GetList<JsonObject>("array").Should().HaveCount(2);
+            actual.GetValuesFromReference("ref").Should().HaveCount(2);
+        }
+    }
+}

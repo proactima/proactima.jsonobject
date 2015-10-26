014f19d2bd7c4e2bf9de411b8cf15e8406dd860f
noocyte <jarle.nygaard@outlook.com>
2015-10-26 13:25:25 +0100

    Now base64 encoding/decoding the jsonobject before serialize/deserialize

--- proactima.jsonobject.tests/DescribeJsonObjectSerialization.cs
+++ proactima.jsonobject.tests/DescribeJsonObjectSerialization.cs
@@ -1,10 +1,53 @@
-﻿using FluentAssertions;
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using FluentAssertions;
 using Xunit;
-using proactima.jsonobject;
+
 namespace proactima.jsonobject.tests
 {
     public class DescribeJsonObjectSerialization
     {
+        [Fact]
+        public void ItShouldSerializeAsBase64EncodedString()
+        {
+            var json = new JsonObject
+            {
+                ["test"] = "1 < 2 > 1"
+            };
+            var x = new XmlSerializer(typeof (JsonObject));
+
+            var ms = new MemoryStream();
+            var txtW = new StreamWriter(ms);
+            x.Serialize(txtW, json);
+            ms.Position = 0;
+            var de = (JsonObject) x.Deserialize(ms);
+            de["test"].Should().Be("1 < 2 > 1");
+        }
+
+
+        [Fact]
+        public void ItShouldDeserializeAsRawString()
+        {
+            var x = new XmlSerializer(typeof (JsonObject));
+
+            var xml = new XmlDocument();
+            var dec = xml.CreateXmlDeclaration("1.0", "utf - 16", null);
+
+            var el = xml.CreateElement("JsonObject");
+            el.InnerText = "{ \"test\":\"some random string\"}";
+
+            xml.AppendChild(dec);
+            xml.AppendChild(el);
+
+            var asString = xml.OuterXml;
+            var txtR = new StringReader(asString);
+            var de = (JsonObject) x.Deserialize(txtR);
+
+            de["test"].Should().Be("some random string");
+        }
+
+
         [Fact]
         public void ItShouldDataContractSerialize_GivenComplexObject()
         {
@@ -24,7 +67,7 @@ namespace proactima.jsonobject.tests
 
             // w
             var actual = WcfTestHelper.DataContractSerializationRoundTrip(complexObject);
-            
+
             // t
             actual.GetList<JsonObject>("array").Should().HaveCount(2);
             actual.GetValuesFromReference("ref").Should().HaveCount(2);
